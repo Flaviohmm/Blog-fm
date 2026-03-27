15e92cab0bf01eee8875dbf9be9db47e60fdad6c
Flavio Macedo <fhenrique609@gmail.com>
2026-03-27 16:14:38 -0300

    Criando a página blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "blog"
 
 urlpatterns = [
-    path("", views.HomeView.as_view(), name="index")
+    path("", views.HomeView.as_view(), name="index"),
+    path("blog/", views.BlogListView.as_view(), name="blog_list"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from typing import Any
 
-from django.views.generic import TemplateView
+from django.db.models.query import QuerySet
+from django.views.generic import TemplateView, ListView
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Category
 
 
 class HomeView(TemplateView):
@@ -42,3 +43,19 @@ class HomeView(TemplateView):
         context['featured_posts'] = Post.objects.filter(published_at__isnull=False).order_by('-published_at')[:3]
 
         return context
+    
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'blog/blog_list.html'
+    context_object_name = 'posts'
+    paginate_by = 9
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return Post.objects.select_related('category').prefetch_related('tags').order_by('-published_at')
+    
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
+    
